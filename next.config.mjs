2fb8055a7394ea3fa0bbf76f1764d1df0a5e7a22
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2024-05-16 17:12:12 +0500

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+export default function ({ defaultConfig }) {
+  const domain = process.env.DOMAIN;
 
-export default nextConfig;
+  let envConfig;
+  switch (domain) {
+    case "domain1":
+      envConfig = import("./.env.domain1").then((module) => module.default);
+      break;
+    case "domain2":
+      envConfig = import("./.env.domain2").then((module) => module.default);
+      break;
+  }
+
+  return {
+    ...defaultConfig,
+    async env() {
+      return {
+        ...defaultConfig.env,
+        ...(await envConfig),
+      };
+    },
+  };
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function Home() {
   console.log(process.env.DOMAIN);
 
